9dcca6ee5449cd1e68d079825481284d4d3fb82d
Asad Hameed <asadhameed2002@yahoo.com>
2021-07-16 18:20:00 +0200

    Add image through imagePicker

--- src/components/ModalComponent.js
+++ src/components/ModalComponent.js
@@ -8,7 +8,11 @@ import {
   TextInput,
   Dimensions,
   ScrollView,
+  Image,
 } from "react-native";
+
+import { Picker } from "@react-native-picker/picker";
+import * as ImagePicker from "expo-image-picker";
 
 import BackGroundImage from "./BackGroundImage";
 import globalStyles from "../constant/stylesSheet";
@@ -19,7 +23,18 @@ const ModalComponent = (props) => {
   const [title, setTitle] = useState();
   const [price, setPrice] = useState();
   const [description, setDescription] = useState();
+  const [eventType, setEventType] = useState("Running");
+  const [image, setImage] = useState();
+  const onPickImageHandler = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
 
+      quality: 1,
+    });
+    console.log(result);
+    if (!result.cancelled) setImage(result.uri);
+  };
   const onSaveHandler = () => {
     console.log(
       "file: ModalComponent.js ~ line 25 ~ ModalComponent ~ title",
@@ -38,17 +53,17 @@ const ModalComponent = (props) => {
     setDescription("");
   };
   return (
-    <Modal
-      transparent={true}
-      animationType="slide"
-      onRequestClose={props.onCloseModal}
-      visible={props.modalVisible}
-    >
-      <ScrollView style={styles.centeredView}>
+    <ScrollView style={styles.centeredView}>
+      <Modal
+        transparent={true}
+        animationType="slide"
+        onRequestClose={props.onCloseModal}
+        visible={props.modalVisible}
+      >
         <View style={styles.modalView}>
-          <BackGroundImage>
+          <BackGroundImage imageBackground={styles.imageBackground}>
             <Text style={globalStyles.titleText}>Creat a new Event</Text>
-            <View style={globalStyles.form}>
+            <ScrollView style={globalStyles.form}>
               <View>
                 <Text style={globalStyles.labelText}>Title</Text>
                 <TextInput
@@ -73,13 +88,47 @@ const ModalComponent = (props) => {
                 <Text style={globalStyles.labelText}>Price</Text>
                 <TextInput
                   style={{ ...globalStyles.input, height: 40 }}
-                  placeholder="Event Price"
+                  placeholder="Event Price in $00,00"
                   placeholderTextColor="rgba(256,256,256,0.4)"
                   autoCapitalize="none"
                   keyboardType="numeric"
                   value={price}
                   onChangeText={setPrice}
                 />
+              </View>
+              <Text style={globalStyles.labelText}>Event Type</Text>
+              <View style={styles.pickerContainer}>
+                <Picker
+                  onValueChange={(value) => setEventType(value)}
+                  selectedValue={eventType}
+                  style={styles.picker}
+                  mode="dropdown"
+                >
+                  <Picker.Item label="Running" value="running" />
+                  <Picker.Item label="Walking" value="walking" />
+                  <Picker.Item label="Swimming" value="swimming" />
+                  <Picker.Item label="Cycling" value="cycling" />
+                </Picker>
+              </View>
+              <View style={styles.imageContainer}>
+                <TouchableOpacity onPress={onPickImageHandler}>
+                  <Text
+                    style={{
+                      ...globalStyles.buttonStyle,
+                      width: "100%",
+                      marginVertical: 5,
+                    }}
+                  >
+                    Event Image
+                  </Text>
+                </TouchableOpacity>
+                {image && (
+                  <Image
+                    source={{ uri: image }}
+                    style={styles.image}
+                    resizeMode="center"
+                  />
+                )}
               </View>
 
               <View style={styles.buttonContainer}>
@@ -103,11 +152,11 @@ const ModalComponent = (props) => {
                   <Text style={globalStyles.buttonStyle}>Save</Text>
                 </TouchableOpacity>
               </View>
-            </View>
+            </ScrollView>
           </BackGroundImage>
         </View>
-      </ScrollView>
-    </Modal>
+      </Modal>
+    </ScrollView>
   );
 };
 const styles = StyleSheet.create({
@@ -122,10 +171,48 @@ const styles = StyleSheet.create({
     borderColor: "white",
     overflow: "hidden",
   },
+  // container: {
+  //   flex: 1,
+  //   alignItems: "flex-start",
+  // },
+  imageBackground: {
+    //  width: windowWidth,
+    // height: windowHight,
+    justifyContent: "flex-start",
+  },
   buttonContainer: {
-    marginTop: 30,
+    marginTop: 20,
+    marginBottom: 40,
     flexDirection: "row",
     justifyContent: "space-between",
+  },
+  imageContainer: {
+    borderWidth: 1,
+    borderColor: "#ffffff",
+    borderRadius: 4,
+    backgroundColor: "rgba(0,0,0,0.3)",
+    marginTop: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    //  height: 200,
+  },
+  image: {
+    height: 200,
+    width: 200,
+    flex: 1,
+    alignSelf: "center",
+    margin: 5,
+    borderWidth: 1,
+    borderColor: "white",
+  },
+  picker: { flex: 1, ...globalStyles.labelText },
+  pickerContainer: {
+    height: 50,
+    borderColor: "#ffffff",
+    backgroundColor: "rgba(0,0,0,0.3)",
+    borderWidth: 1,
+    borderRadius: 4,
   },
 });
 
